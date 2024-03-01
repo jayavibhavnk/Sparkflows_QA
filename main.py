eb959ec4d0944e3a2ee872d9e098d9ba9d648446
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-03-01 15:49:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -83,11 +83,7 @@ def query_from_doc2(text):
 
 def query_from_doc(text):
     response = st.session_state.conversation({"question": text, "chat_history": st.session_state.chat_history})
-    # st.session_state.chat_history.append(response["chat_history"][0])
-    # st.session_state.chat_history.append(response["chat_history"][1])
-    # # st.session_state.chat_history.append("yo")
     st.state_session.chat_history = response['chat_history']
-    # return response['chat_history'] 
     return response['answer']
 
 def query_with_link(query):
